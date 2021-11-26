f77a07a7d31f641161371034d20cca980a790bf4
superxian666 <1259346558@qq.com>
2021-11-26 08:53:41 +0800

    update

--- auto2021/src/test/supeixian/demo/test.java
+++ auto2021/src/test/supeixian/demo/test.java
@@ -3,13 +3,42 @@ package test.supeixian.demo;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class test {
-	public static void main(String[] args) {
-		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
-		Date date1 = new Date();
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.webtest.core.BaseTest;
+
+public class test extends BaseTest{
+//	登录的方法，方便后面复用，减少代码重复量
+	public void login(String username,String password) {
+		webtest.open("");
+//		webtest.open("localhost/jianyuluntan_2.4.6/");
 		
-		System.out.println(dateFormat.format(date1));
+		webtest.click("link=登录");
+		webtest.click("name=user");
+		webtest.type("name=user",username);
 		
+		webtest.click("name=pwd");
+		webtest.type("name=pwd", password);
+
+		webtest.click("id=submit");
+
+	}
+	
+	@Test
+	public void test1() {
+
+		login("superxian","123456789spx");
+		
+//		webtest.click("link=superxian");
+//		webtest.click("link=我的收藏");
+//		
+//		String url = webtest.getUrl();
+//		Assert.assertEquals("http://localhost/jianyuluntan_2.4.6/index.php/user/index/mycollection.html",url);
+//		System.out.println("test01成功");
+//		
+//		driver.quit();
+
 	}
 
 
